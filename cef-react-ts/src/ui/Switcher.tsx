0a8d637f659dd2bc5a98137bc170ec50c8f8fba1
Sooliks <evgenij.terexov.2000@mail.ru>
2023-08-08 02:48:35 +0300

    gjjggj

--- cef-react-ts/src/ui/Switcher.tsx
+++ cef-react-ts/src/ui/Switcher.tsx
@@ -3,7 +3,7 @@ import {Button, Space, Typography} from "antd";
 import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 
 
-const {Title} = Typography;
+const {Title, Text} = Typography;
 
 export type DataTypeSwitcher = {
     value: number
@@ -34,13 +34,15 @@ const Switcher: React.FC<SwitcherType> = ({data,onChange,width,text}) => {
 
     return (
         <Space direction={"vertical"}>
-            <Title style={{textAlign: 'center', margin: 0}} level={4}>{text}</Title>
+            <Typography.Title level={5} style={{textAlign: 'center', margin: 0}}>
+                {text}
+            </Typography.Title>
             <Space align={"start"} style={{justifyContent: 'space-between', width: width === undefined ? 280 : width, marginLeft: 10, marginRight: 10}}>
                 <Button disabled={currentValue===0} icon={<LeftOutlined />} onClick={handleClickLeft}/>
-                <div style={{border: '1px solid #d9d9d9', width: 200, height: 30}}>
-                    <Typography.Title level={5} style={{textAlign: 'center', margin: 0}}>
+                <div style={{border: '1px solid #d9d9d9', width: 200, height: 30, display: "flex", flexDirection: "column", justifyContent: 'center'}}>
+                    <Text style={{textAlign: 'center', margin: 0}}>
                         {data[currentValue].placeHolder}
-                    </Typography.Title>
+                    </Text>
                 </div>
                 <Button disabled={currentValue===data.length-1} icon={<RightOutlined />} onClick={handleClickRight}/>
             </Space>
