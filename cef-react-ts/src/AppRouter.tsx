2bf15033d9623847fd511995edf25dbac997bd76
Sooliks <evgenij.terexov.2000@mail.ru>
2023-08-08 04:12:56 +0300

    gkgk

--- cef-react-ts/src/AppRouter.tsx
+++ cef-react-ts/src/AppRouter.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Auth from "./components/auth/Auth";
 import CreateCharacter from "./components/character/createCharacter/CreateCharacter";
+import SelectCharacters from "./components/character/selectCharacters/SelectCharacters";
 
 const AppRouter: React.FC = () => {
     return (
         <Routes>
             <Route path={"/auth"} element={<Auth/>}/>
             <Route path={"/createcharacter"} element={<CreateCharacter/>}/>
+            <Route path={"/selectcharacters"} element={<SelectCharacters/>}/>
         </Routes>
     );
 };
--- cef-react-ts/src/components/character/selectCharacters/SelectCharacters.tsx
+++ cef-react-ts/src/components/character/selectCharacters/SelectCharacters.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Config} from "../../../conf";
+import {Button, Card, Descriptions, DescriptionsProps, Space, Tooltip} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
 
 const SelectCharacters : React.FC = () => {
-    return (
-        <div>
+    const [characters,setCharacters] = useState<DescriptionsProps['items'][]>([
+        [
+            {
+                label: 'Имя',
+                children: 'Zhou',
+            },
+            {
+                label: 'Фамилия',
+                children: 'Babrov',
+            },
+            {
+                label: 'Уровень',
+                children: '8',
+            },
+            {
+                label: 'Денег в банке',
+                children: '1000$',
+            },
+            {
+                label: 'Наличные',
+                children: '10000$',
+            },
+        ],
+        [
 
-        </div>
+        ],
+        [
+
+        ]
+    ])
+
+
+    return (
+        <Space align={"center"} direction={"vertical"} style={{position:'absolute',width:Config.screenResolution.width, height:Config.screenResolution.height, justifyContent: 'center'}}>
+            <Space>
+                <Card title={"Персонажи"}>
+                    <Space style={{width: 1300, height: 700, justifyContent: 'space-around'}}>
+                        {characters.map((character,index)=>
+                            <Card key={index}>
+                                <Space align={character?.length === 0 ? 'center' : undefined} direction={"vertical"} style={{justifyContent: character?.length === 0 ? 'center' : 'space-between',height: 500, width: 380}}>
+                                    {character?.length !== 0 && <Descriptions column={1} title="Информация о персонаже" items={characters[index]} />}
+                                    {character?.length === 0 ?
+                                        <Tooltip title={"Создать"}>
+                                            <Button size={"large"} icon={<PlusOutlined />} style={{alignSelf: 'center'}}></Button>
+                                        </Tooltip>
+                                        :
+                                        <Button style={{width: '100%'}}>Войти</Button>
+                                    }
+                                </Space>
+                            </Card>
+                        )}
+                    </Space>
+                </Card>
+            </Space>
+        </Space>
     );
 };
 
