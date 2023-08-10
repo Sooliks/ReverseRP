14f6ec0c1361f8d3706bc4769f5173c8b65b0ee0
Sooliks <evgenij.terexov.2000@mail.ru>
2023-08-10 23:12:16 +0300

    inventory ready

--- cef-react-ts/src/components/inventory/Inventory.tsx
+++ cef-react-ts/src/components/inventory/Inventory.tsx
@@ -15,7 +15,8 @@ const Inventory : React.FC = () => {
 
     const [inventoryPlayer,setInventoryPlayer] = useState<ItemType[]>([
         {id: 0, count: 5, description: 'Восполняет 40 еды', name: "burger", currentBoard: inventoryContext.inventory.boardsPlayer[0], index: 0},
-        {id: 0, count: 5, description: 'Восполняет 40 еды', name: "бигтейсти", currentBoard: inventoryContext.inventory.boardsPlayer[1], index: 1}
+        {id: 0, count: 5, description: 'Восполняет 40 еды', name: "бигтейсти", currentBoard: inventoryContext.inventory.boardsPlayer[1], index: 1},
+        {id: 5, count: 5, description: 'Восполняет 40 еды', name: "Телефон", currentBoard: inventoryContext.inventory.boardsPlayer[2], index: 2}
     ])
     const [inventoryClothes,setInventoryClothes] = useState<ItemType[]>([])
     const [inventoryOther,setInventoryOther] = useState<ItemType[]>([])
@@ -23,15 +24,18 @@ const Inventory : React.FC = () => {
 
 
     useEffect(()=>{
-        let newBoards: BoardType[] = inventoryContext.inventory.boardsPlayer;
         //делаем клетки для айтемов
+        let newBoards: BoardType[] = inventoryContext.inventory.boardsPlayer;
         for(let i = 0; i < 56; i++){
             newBoards = [...newBoards,{type: BoardTypeEnum.Player, idBoard: i}]
         }
-        //делаем клетки одежды для айтемов
         let newBoardsClothes: BoardType[] = inventoryContext.inventory.boardsClothes;
         for(let i = 56; i < 63; i++){
             newBoardsClothes = [...newBoardsClothes,{type: BoardTypeEnum.ClothesPlayer, idBoard: i}]
+        }
+        let newBoardsOther: BoardType[] = inventoryContext.inventory.boardsOther;
+        for(let i = 63; i < 90; i++){
+            newBoardsOther = [...newBoardsOther,{type: BoardTypeEnum.Other, idBoard: i}]
         }
         /////////////////////////////////////////////////////////////////////////////////////////////////////
         //добавляем айтемы игрока в борды
@@ -51,7 +55,15 @@ const Inventory : React.FC = () => {
                 newBoardsClothes[i].item.currentBoard = newBoardsClothes[inventoryClothes[i].index];
             }
         }
-        inventoryContext.setInventory({...inventoryContext.inventory,boardsPlayer: newBoards, boardsClothes: newBoardsClothes})
+        //заполняем борды другого инвентаря айтемами
+        for (let i = 0; i < inventoryOther.length;i++){
+            newBoardsOther[inventoryOther[i].index].item = inventoryOther[i];
+            if(newBoardsOther[i].item!==undefined){
+                // @ts-ignore
+                newBoardsOther[i].item.currentBoard = newBoardsOther[inventoryOther[i].index];
+            }
+        }
+        inventoryContext.setInventory({...inventoryContext.inventory,boardsPlayer: newBoards, boardsClothes: newBoardsClothes, boardsOther: newBoardsOther})
     },[])
 
     const getTypeBoardById = (idBoard?: number): BoardTypeEnum =>{
@@ -75,41 +87,31 @@ const Inventory : React.FC = () => {
         const newBoardId: number = parseInt(result.destination.droppableId); // новый борд его id
         const itemDraggable = inventoryPlayer[parseInt(result.draggableId)]; // айтем который двигаем
         if(itemDraggable.currentBoard!==undefined){
-            console.log(result)
             changeItem(newBoardId, itemDraggable)
         }
     }
     const changeItem = (newBoardId: number, itemDraggable: ItemType) => {
         let newBoards: BoardType[] = [];
-
         if(getTypeBoardById(newBoardId)===BoardTypeEnum.Player)newBoards = inventoryContext.inventory.boardsPlayer;
         if(getTypeBoardById(newBoardId)===BoardTypeEnum.ClothesPlayer)newBoards = inventoryContext.inventory.boardsClothes;
         if(getTypeBoardById(newBoardId)===BoardTypeEnum.Other)newBoards = inventoryContext.inventory.boardsOther;
         const indexNewBoard = newBoards.indexOf(newBoards.filter(b=>b.idBoard === newBoardId)[0])
 
-        // @ts-ignore
-        switch (itemDraggable.currentBoard.type){
+        switch (itemDraggable.currentBoard!.type){
             case BoardTypeEnum.Player:
                 const clearingBoardsPlayer: BoardType[] = inventoryContext.inventory.boardsPlayer;
-                console.log(clearingBoardsPlayer[clearingBoardsPlayer.indexOf(itemDraggable.currentBoard!)].item )
-                // @ts-ignore
-                clearingBoardsPlayer[clearingBoardsPlayer.indexOf(itemDraggable.currentBoard)].item = undefined;
+                clearingBoardsPlayer[clearingBoardsPlayer.indexOf(itemDraggable.currentBoard!)].item = undefined;
                 inventoryContext.setInventory({...inventoryContext.inventory,boardsPlayer: clearingBoardsPlayer})
-                //newBoards = clearingBoardsPlayer;
                 break
             case BoardTypeEnum.ClothesPlayer:
                 const clearingBoardsClothesPlayer: BoardType[] = inventoryContext.inventory.boardsClothes;
-                // @ts-ignore
-                clearingBoardsClothesPlayer[clearingBoardsClothesPlayer.indexOf(itemDraggable.currentBoard)].item = undefined;
+                clearingBoardsClothesPlayer[clearingBoardsClothesPlayer.indexOf(itemDraggable.currentBoard!)].item = undefined;
                 inventoryContext.setInventory({...inventoryContext.inventory,boardsClothes: clearingBoardsClothesPlayer})
-                //newBoards = clearingBoardsClothesPlayer;
                 break
             case BoardTypeEnum.Other:
                 const clearingBoardsOther: BoardType[] = inventoryContext.inventory.boardsOther;
-                // @ts-ignore
-                clearingBoardsOther[clearingBoardsOther.indexOf(itemDraggable.currentBoard)].item = undefined;
+                clearingBoardsOther[clearingBoardsOther.indexOf(itemDraggable.currentBoard!)].item = undefined;
                 inventoryContext.setInventory({...inventoryContext.inventory,boardsOther: clearingBoardsOther})
-                //newBoards = clearingBoardsOther;
                 break
         }
         newBoards[indexNewBoard].item = {id: 0, count: 5, description: 'dg', name: 'dgdg', index: 0}
