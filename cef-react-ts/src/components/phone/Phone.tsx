b2ea45464a7078dc43a1df00a930f810d76d9ae3
Sooliks <evgenij.terexov.2000@mail.ru>
2023-08-16 00:10:10 +0300

    ihghghhfhfар

--- cef-react-ts/src/components/phone/Phone.tsx
+++ cef-react-ts/src/components/phone/Phone.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Config} from "../../conf";
 import {Space} from "antd";
+import img from '../../assets/images/phone.png'
+
 
 const Phone: React.FC = () => {
     return (
         <Space style={{width: Config.screenResolution.width, height: Config.screenResolution.height, position: 'absolute', justifyContent: 'end', alignItems: 'end'}}>
-            <Space>
-                <h1>fgfh</h1>
-            </Space>
+            <div style={{background: `url(${img}) no-repeat center/cover`, width: 300, height: 500, display: 'flex', justifyContent: 'center'}}>
+                <div style={{border: '2px solid black', width: 221, height: 362, marginTop: 64, marginRight: 2}}>
+
+                </div>
+            </div>
         </Space>
     );
 };
