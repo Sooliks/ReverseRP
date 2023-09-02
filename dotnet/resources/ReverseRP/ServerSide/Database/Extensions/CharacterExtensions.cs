d4a3881505dcf71390cdd64dd1cedcfdc333254d
Sooliks <evgenij.terexov.2000@mail.ru>
2023-09-03 02:39:41 +0300

    fhhf

---
+++ dotnet/resources/ReverseRP/ServerSide/Database/Extensions/CharacterExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ServerSide.Database.Handlers;
+using ServerSide.Database.Models;
+
+namespace ServerSide.Database.Extensions;
+
+public static class CharacterExtensions
+{
+    public static List<ItemBase> GetInventory(this Character character) => InventoryHandler.GetInventory(character);
+    public static void AddItem(this Character character, ItemBase item) => InventoryHandler.AddItem(character, item);
+    public static void RemoveItem(this Character character, ItemBase item) => InventoryHandler.RemoveItem(character, item);
+}
