cdffb0ab1dc65202c056737426cccc033c447d74
Sooliks <evgenij.terexov.2000@mail.ru>
2023-09-27 18:34:34 +0300

    gjgjgj

--- cef-react-ts/src/App.tsx
+++ cef-react-ts/src/App.tsx
@@ -4,19 +4,44 @@ import NavigationContextProvider from "./context/NavigationContextProvider";
 import Hud from "./components/hud/Hud";
 import AppRouter from "./AppRouter";
 import {Config} from "./conf";
+import {notification} from "antd";
 
 
 
 const App = () => {
-    useEffect(()=>{
-        /*notification.error({
-            message: "Уведомление",
-            description: "Подтвердите аккаунт в настройках, иначе не сможете восстановить его в случае утери"
-        })*/
-    },[])
+    mp.events.add("SERVER:CEF::NOTIFY", (json: string)=>{
+        const args: any[] = JSON.parse(json);
+        const type: number = args[0];
+        const message: string = args[1];
+        switch (type){
+            case 0:
+                notification.error({
+                    message: "Уведомление",
+                    description: message
+                })
+                break
+            case 1:
+                notification.success({
+                    message: "Уведомление",
+                    description: message
+                })
+                break
+            case 2:
+                notification.info({
+                    message: "Уведомление",
+                    description: message
+                })
+                break
+            case 3:
+                notification.warning({
+                    message: "Уведомление",
+                    description: message
+                })
+                break
+        }
+    })
 
-
-
+    
     return (
         <NavigationContextProvider>
             <div style={{width:Config.screenResolution.width, height:Config.screenResolution.height, backgroundImage: Config.isDevelopment ? `url('https://www.digiseller.ru/preview/858000/p1_3575817_2fcd5317.jpg')` : ``}}>
---
+++ dotnet/resources/ReverseRP/ServerSide/Services/MapService/InputMarker.cs
@@ -0,0 +1,32 @@
+﻿using GTANetworkAPI;
+using ServerSide.Extensions;
+
+namespace ServerSide.Services.MapService;
+
+public class InputMarker : Script
+{
+        private static readonly string ColShapeCefPathKey = nameof(ColShapeCefPathKey);
+        public static void CreateDefaultInputMarkerWithOpenCefPath(string textLabel, Vector3 pos, int iconBlip, byte colorBlip, string nameCefPath)
+        {
+            float posX = pos.X;
+            float posY = pos.Y;
+            float posZ = pos.Z;
+            NAPI.TextLabel.CreateTextLabel(textLabel, new Vector3(posX, posY, posZ + 0.8f), 20.0f, 0.75f, 4, new Color(255, 255, 255));//Основной тект
+            NAPI.Marker.CreateMarker(MarkerType.VerticalCylinder, new Vector3(posX, posY, posZ - 1), new Vector3(), new Vector3(), 1.0f, new Color(255, 255, 255));//Маркер
+            Blip blip = NAPI.Blip.CreateBlip(iconBlip, new Vector3(posX, posY, posZ), 1.0f, colorBlip);//Блип
+            NAPI.Blip.SetBlipName(blip, textLabel);//Установка имени блипу
+            NAPI.Blip.SetBlipShortRange(blip, true); //Установка видимости блипу
+            var colShape = NAPI.ColShape.CreateCylinderColShape(pos, 1.0f, 1.0f);
+            colShape.SetData(ColShapeCefPathKey,nameCefPath);
+        }
+    
+        [ServerEvent(Event.PlayerEnterColshape)]
+        public static void OnPlayerEnterColShape(ColShape colShape, Player player)
+        {
+            if (colShape.HasData(ColShapeCefPathKey))
+            {
+                if(player.IsInVehicle)return;
+                player.ChangeCefWindow(colShape.GetData<string>(ColShapeCefPathKey));
+            }
+        }
+}
