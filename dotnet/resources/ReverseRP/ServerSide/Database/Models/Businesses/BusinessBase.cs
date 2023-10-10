3ca19d95cb4bd7cddd24a1caada5b2216372ed2d
Sooliks <evgenij.terexov.2000@mail.ru>
2023-10-10 17:32:58 +0300

    hghgh

--- dotnet/resources/ReverseRP/ServerSide/Database/Models/Businesses/BusinessBase.cs
+++ dotnet/resources/ReverseRP/ServerSide/Database/Models/Businesses/BusinessBase.cs
@@ -5,6 +5,7 @@ public class BusinessBase
     public int Id { get; set; }
     public Character? OwnerCharacter { get; set; }
     public int Bank { get; set; }
+    public int GosPrice { get; set; }
 
     public BusinessBase()
     {
---
+++ dotnet/resources/ReverseRP/ServerSide/EventsHandlers/BusinessesEvents/EventsBuyBusinesses.cs
@@ -0,0 +1,12 @@
+﻿using GTANetworkAPI;
+
+namespace ServerSide.EventsHandlers.BusinessesEvents;
+
+public class EventsBuyBusinesses
+{
+    [RemoteEvent("CEF::SERVER:ON_BUY_BUSINESS")]
+    public void OnBuyBusiness(Player player)
+    {
+        
+    }
+}
