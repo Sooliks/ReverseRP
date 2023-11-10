3f17f8e7edf72fe7534d3690f4d29710579a6e6e
Sooliks <evgenij.terexov.2000@mail.ru>
2023-11-10 16:38:39 +0300

    products half ready

--- cef-react-ts/src/components/management-business/Products.tsx
+++ cef-react-ts/src/components/management-business/Products.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Client} from "../../requests/Client";
 import {ServerData} from "../../data/ServerData";
+import {Space, Tag, Typography} from "antd";
+import {ColumnsType} from "antd/es/table";
+import {Table} from "antd/lib";
 
 
 
@@ -23,6 +26,37 @@ type ProductType = {
     itemBusiness: ItemBusinessType
 
 }
+const {Text,Link} = Typography;
+
+const columns: ColumnsType<ProductType> = [
+    {
+        title: 'Название',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Кол-во',
+        dataIndex: 'count',
+        key: 'count',
+        render: (_, record) => (
+            <Space size="middle">
+                <Text>{record.itemBusiness.count + ' шт.'}</Text>
+                <Link>Заказать</Link>
+            </Space>
+        ),
+    },
+    {
+        title: 'Цена',
+        dataIndex: 'price',
+        key: 'price',
+        render: (_, record) => (
+            <Space size="middle">
+                <Text>{record.itemBusiness.price + '$'}</Text>
+                <Link>Изменить</Link>
+            </Space>
+        ),
+    },
+];
 
 const Products: React.FC<ProductsProps> = ({idBusiness}) => {
     useEffect(()=>{
@@ -33,6 +67,13 @@ const Products: React.FC<ProductsProps> = ({idBusiness}) => {
                 case "Автосалон высокого класса":
                     Client.callProcServer<string>("RPC::CEF::SERVER:GetVehiclesTypes").then(data=>{
                         ServerData.vehiclesTypes = JSON.parse(data);
+                        incomingData.items.map(item=>{
+                            setProducts([...products,
+                                {
+                                    name: ServerData.vehiclesTypes.find(v=>v.Id == item.itemId)!.Mark,
+                                    itemBusiness: item
+                                }])
+                        })
                     })
                     break
                 case "Заправка":
@@ -46,7 +87,7 @@ const Products: React.FC<ProductsProps> = ({idBusiness}) => {
 
     return (
         <div style={{width: '100%', height: '100%'}}>
-
+            <Table columns={columns} dataSource={products}/>
         </div>
     );
 };
