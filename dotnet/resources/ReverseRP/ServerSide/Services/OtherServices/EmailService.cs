58cb1773732ff8ac99cdfe4ee46fc67463e37097
Sooliks <104236081+Sooliks@users.noreply.github.com>
2025-06-01 05:47:30 +0300

    Update EmailService.cs

--- dotnet/resources/ReverseRP/ServerSide/Services/OtherServices/EmailService.cs
+++ dotnet/resources/ReverseRP/ServerSide/Services/OtherServices/EmailService.cs
@@ -6,8 +6,8 @@ namespace ServerSide.Services.OtherServices;
 
 public class EmailService
 {
-    private static string _mail = "sooliks@mail.ru";
-    private static string _password = "pQD3qfjVhM0cxpPLkzNE";
+    private static string _mail = "";
+    private static string _password = "";
     private static string _username = "ReverseRP";
     private static string _host = "smtp.mail.ru";
     private static int _port = 587;
