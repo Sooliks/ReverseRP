942083f88aa7949de214653b12a20dfe7d0645bd
Sooliks <104236081+Sooliks@users.noreply.github.com>
2025-06-01 05:47:55 +0300

    Update DiscordBot.cs

--- dotnet/resources/ReverseRP/ServerSide/Discord/DiscordBot.cs
+++ dotnet/resources/ReverseRP/ServerSide/Discord/DiscordBot.cs
@@ -16,7 +16,7 @@ public class DiscordBot
     public static DiscordSocketClient Client;
     public readonly static ulong ChannelDevId = 1157652166580908052;
     public readonly static ulong ChannelGameLogsId = 1080966670329335929;
-    private const string Token = "MTAzOTkxNjIwMzQyODMwMjkwOA.Gz79Ss.dv_T2e4_n4Gy4eMfxw6NzrGHwmdVrQIZ8u5pBU";
+    private const string Token = "";
     private const ulong Guild = 1039165715623710790;
     public static async Task StartDiscordBot()
     {
